343cacee28050344c4e97845412d28a2f00bc627
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-09-02 09:02:06 -0300

    fix: close side menu on click option

--- components/NewHeader/components/SideMenu/index.tsx
+++ components/NewHeader/components/SideMenu/index.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../../../../@/components/ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../../../../@/components/ui/sheet";
 import { useTranslation } from "react-i18next";
 import { TiThMenu } from "react-icons/ti";
 
@@ -9,24 +9,28 @@ export function SideMenu() {
     return (
         <Sheet>
             <SheetTrigger>
-                <TiThMenu size={25} color='#2b2b2b'/>
+                <TiThMenu size={25} color='#2b2b2b' />
             </SheetTrigger>
 
             <SheetContent className='w-[300px] absolute right-0 top-0 bg-white p-5 flex flex-col gap-5'>
                 <nav className="flex flex-col gap-5 mt-14">
-                    <Link
-                        className="font-semibold text-[#2b2b2b]"
-                        href='/blog'
-                    >
-                        Blog
-                    </Link>
+                    <SheetClose asChild>
+                        <Link
+                            className="font-semibold text-[#2b2b2b]"
+                            href='/blog'
+                        >
+                            Blog
+                        </Link>
+                    </SheetClose>
 
-                    <Link
-                        className="font-semibold text-[#2b2b2b]"
-                        href='#solutions'
-                    >
-                        {t('solucoes')}
-                    </Link>
+                    <SheetClose asChild>
+                        <Link
+                            className="font-semibold text-[#2b2b2b]"
+                            href='#solutions'
+                        >
+                            {t('solucoes')}
+                        </Link>
+                    </SheetClose>
                 </nav>
             </SheetContent>
         </Sheet>
