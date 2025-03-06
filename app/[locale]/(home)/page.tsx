0b2bd76819713386b84a41ac62615e2a5070b9e3
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-03-06 14:15:39 -0300

    feat: init shadcn and add button component

--- app/[locale]/(home)/page.tsx
+++ app/[locale]/(home)/page.tsx
@@ -1,5 +1,6 @@
 import initTranslations from '../../i18n';
 import TranslationsProvider from '../../../components/TranslationsProvider';
+import { Button } from '@/components/ui/button';
 
 const i18nNamespaces = ['home'];
 
@@ -15,6 +16,7 @@ export default async function Home({ params: { locale } }: { params: { locale: s
             <div>
                 <h1 className='text-green-500 text-xs'>{t('hello')} ola</h1>
                 <p className='text-xl'>teste</p>
+                <Button>Teste</Button>
             </div>
         </TranslationsProvider>
     )
