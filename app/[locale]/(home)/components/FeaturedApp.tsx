f6af7ae9ae8663779a79939f2163a8c7ff44180d
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-03-16 17:39:41 -0300

    fix: responsive featured app

--- app/[locale]/(home)/components/FeaturedApp.tsx
+++ app/[locale]/(home)/components/FeaturedApp.tsx
@@ -10,7 +10,7 @@ export function FeaturedApp({ t }: Props) {
     return (
         <section className="w-full mt-10 lg:mt-20 bg-green-3 py-10">
             <div className="container mx-auto px-5 lg:px-20 flex flex-wrap justify-between gap-5">
-                <div className="flex flex-col w-full md:max-w-[48%]">
+                <div className="flex flex-col w-full lg:max-w-[48%]">
                     <h3 className="text-2xl md:text-4xl text-green-1">{t('featuredApp')}</h3>
                     <h4 className="text-white text-2xl mt-3">{t('regenerationCredit')}</h4>
 
@@ -37,14 +37,14 @@ export function FeaturedApp({ t }: Props) {
                     </div>
                 </div>
 
-                <div className="flex flex-col items-end w-full md:max-w-[48%] md:pr-20">
+                <div className="hidden flex-col items-center justify-center w-full lg:max-w-[48%] lg:flex">
                     <Image
                         src={ImageFeatureApp}
-                        width={500}
-                        height={500}
+                        width={350}
+                        height={350}
                         quality={100}
                         alt="image regeneration credit"
-                        className="w-full h-full object-contain md:w-[350px] md:h-[350px]"
+                        className="w-[400px] h-[400px] object-contain"
                     />
                 </div>
             </div>
