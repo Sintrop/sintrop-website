f2eb4942431c9adba96eb32b7da380c611690d31
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-03-16 17:29:37 -0300

    feat: add links footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { TType } from '@/types/t';
 import LanguageChanger from '../LanguageChanger';
+import { Github } from 'lucide-react';
+import DiscordIcon from '@/public/assets/icons/discord-white-icon.png';
 
 interface Props {
     t: TType;
@@ -11,7 +13,7 @@ export function Footer({ t }: Props) {
     return (
         <footer className="bg-[#149954] py-10 lg:py-20">
             <div className="container mx-auto flex flex-col gap-5">
-                <section className="flex flex-col gap-5 items-center justify-around border-b border-white pb-10 lg:flex-row">
+                <section className="flex flex-col gap-5 items-center justify-around border-b border-white pb-10 md:flex-row">
                     <Link
                         className="flex items-center gap-3"
                         href="/"
@@ -24,17 +26,69 @@ export function Footer({ t }: Props) {
                         />
                     </Link>
 
-                    <nav className="flex flex-col gap-5">
-                        <Link
-                            href="/"
-                            className="text-white hover:underline"
-                        >
-                            - {t('home')}
-                        </Link>
-                    </nav>
+                    <div className='flex justify-around w-full md:w-[30%]'>
+                        <nav className="flex flex-col gap-3">
+                            <Link
+                                href="/"
+                                className="text-white hover:underline"
+                            >
+                                - {t('home')}
+                            </Link>
+                            <Link
+                                href="/resources"
+                                className="text-white hover:underline"
+                            >
+                                - {t('resources')}
+                            </Link>
+                            <Link
+                                href="/tutorials"
+                                className="text-white hover:underline"
+                            >
+                                - {t('tutorials')}
+                            </Link>
+                            <Link
+                                href="/about"
+                                className="text-white hover:underline"
+                            >
+                                - {t('about')}
+                            </Link>
+                        </nav>
 
-                    <div className='flex flex-col gap-5'>
-                        <LanguageChanger />
+                        <div className='flex flex-col gap-5'>
+                            <nav className="flex flex-col gap-5">
+                                <Link
+                                    href="https://github.com/sintrop"
+                                    target='_blank'
+                                    className="text-white flex items-center justify-center gap-3 hover:underline"
+                                    rel="noopener noreferer"
+                                >
+                                    <Github size={25} color="white" />
+                                    {t('github')}
+                                </Link>
+
+                                <Link
+                                    href="https://github.com/sintrop"
+                                    target='_blank'
+                                    className="text-white flex items-center justify-center gap-3 hover:underline"
+                                    rel="noopener noreferer"
+                                >
+                                    <Image
+                                        src={DiscordIcon}
+                                        alt="Discord icon"
+                                        quality={100}
+                                        width={25}
+                                        height={25}
+                                    />
+                                    {t('discord')}
+                                </Link>
+                            </nav>
+
+
+                            <div className='flex flex-col items-center gap-1'>
+                                <p className='text-white text-xs'>{t('language')}</p>
+                                <LanguageChanger />
+                            </div>
+                        </div>
                     </div>
                 </section>
 
