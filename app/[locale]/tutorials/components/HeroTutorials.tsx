8345dd151fcb87a6bef6dd36247591bf4826cfa3
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-03-17 15:45:24 -0300

    fix: change h2 to h1

--- app/[locale]/tutorials/components/HeroTutorials.tsx
+++ app/[locale]/tutorials/components/HeroTutorials.tsx
@@ -8,9 +8,9 @@ interface Props {
 export function HeroTutorials({t, title}: Props) {
     return (
         <section className="container mx-auto flex flex-col gap-10 px-5 pb-10 lg:pt-20 lg:px-20 lg:pb-36">
-            <h2 className="text-white text-center font-bold text-4xl md:text-7xl">
+            <h1 className="text-white text-center font-bold text-4xl md:text-7xl">
                 {t(title)}
-            </h2>
+            </h1>
         </section>
     )
 }
