af2db25d8727c41e7fd1faab42be42459621cf5c
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-07-21 16:42:01 -0300

    feat: add button add chain to metamask

---
+++ app/[locale]/(home)/components/AddToMetamask.tsx
@@ -0,0 +1,55 @@
+"use client";
+import MMIcon from "@/public/assets/icons/metamask.png";
+import Image from "next/image";
+import { useTranslation } from "react-i18next";
+
+export function AddToMetamask() {
+  const { t } = useTranslation();
+
+  async function handleAddChain() {
+    if (typeof window !== "undefined") {
+      if (!window.ethereum) return;
+
+      const networkParams = {
+        chainId: "0x3D171",
+        chainName: "Sintrop",
+        nativeCurrency: {
+          name: "SINTROP",
+          symbol: "SINT",
+          decimals: 18,
+        },
+        rpcUrls: ["https://rpc.sintrop.com"],
+        blockExplorerUrls: ["https://explorer.sintrop.com"],
+      };
+
+      await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [networkParams],
+      });
+    }
+  }
+
+  if (typeof window !== "undefined") {
+    if (!window.ethereum) {
+      return <div />;
+    }
+  }
+
+  return (
+    <button
+      onClick={handleAddChain}
+      className="w-full border-2 border-white gap-3 h-[50px] md:h-[60px] rounded-md text-white font-semibold md:w-[220px] flex items-center justify-center hover:cursor-pointer hover:bg-white hover:text-black duration-200"
+    >
+      <Image
+        alt="metamask icon"
+        src={MMIcon}
+        width={40}
+        height={40}
+        quality={100}
+        objectFit="contain"
+      />
+
+      {t("addToMetamask")}
+    </button>
+  );
+}
